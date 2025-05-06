5a9ec4244a407642c20a8eca20868b76520413db
Dmitry <dmitry2595@gmail.com>
2025-05-06 17:41:27 +0300

    feat: banner base css

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,10 +3,10 @@ import Form from './Form'
 
 export default function Banner() {
   return (
-    <>
+    <div className='banner'>
       <h1>Short your link</h1>
-      <h3>Make a simple, clean, short links, that are easy to post, send, and share</h3>
+      <h3>Make a simple, clean, short links, that are easy to post, send and share</h3>
       <Form/>
-    </>
+    </div>
   )
 }
